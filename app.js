5bd9f6a526f6b68b0e347f98934f73239070e7a3
Alix FDM <boujour@alixfdm.fr>
2020-04-01 20:28:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+(function() {
+  let percent = 20;
+
+  setInterval(() => {
+    percent += 0.1;
+    increaseBar(percent);
+  }, 5);
+})();
+
+function increaseBar(percent) {
+  /*
+    // Bonus this is to get the css var from root
+    progressBarValue = getComputedStyle(
+    document.documentElement
+    ).getPropertyValue("--progress-bar-value"); 
+ */
+
+  // This is actualy what we need !
+  document.documentElement.style.setProperty("--progress-bar-value", percent);
+}
