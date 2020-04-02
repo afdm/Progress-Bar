61339bd09dfd88a929206eca228117a99bc74867
Alix FDM <boujour@alixfdm.fr>
2020-04-02 11:52:08 -0300

    update the load event

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-(function() {
+window.addEventListener("load", () => {
   let percent = 20;
 
   setInterval(() => {
     percent += 0.1;
     increaseBar(percent);
   }, 5);
-})();
 
-function increaseBar(percent) {
-  /*
-    // Bonus this is to get the css var from root
-    progressBarValue = getComputedStyle(
-    document.documentElement
-    ).getPropertyValue("--progress-bar-value"); 
- */
+  function increaseBar(percent) {
+    /*
+      // Bonus this is to get the css var from root
+      progressBarValue = getComputedStyle(
+      document.documentElement
+      ).getPropertyValue("--progress-bar-value"); 
+  */
 
-  // This is actualy what we need !
-  document.documentElement.style.setProperty("--progress-bar-value", percent);
-}
+    // This is actualy what we need !
+    document.documentElement.style.setProperty("--progress-bar-value", percent);
+  }
+});
